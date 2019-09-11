5f119a1b2e4704697c30e6c1c2c96b27b4c810a2
Andruszczenko, Eugene <eugene_andruszczenko@manulife.com>
2019-09-11 11:25:32 -0400

    Wed 11 Sep 2019 11:25:32 EDT - Initial Commit

---
+++ example/build/precache-manifest.a108c0d7958b62a7f8a9f3e8bf3f47d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b509a1a636c3512b2e3e7827232e8d",
+    "url": "/hud-gamepad/index.html"
+  },
+  {
+    "revision": "353b6072107ffdda167d",
+    "url": "/hud-gamepad/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "25b7fdcc37171b4dc12e",
+    "url": "/hud-gamepad/static/js/2.a25d00ec.chunk.js"
+  },
+  {
+    "revision": "353b6072107ffdda167d",
+    "url": "/hud-gamepad/static/js/main.03574e43.chunk.js"
+  },
+  {
+    "revision": "f3bbd60516aecd654671",
+    "url": "/hud-gamepad/static/js/runtime~main.c65e3be0.js"
+  }
+]);
